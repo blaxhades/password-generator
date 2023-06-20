6d229a796718e6c568ba364f640eac57f02b2a56
blaxhades <122688263+blaxhades@users.noreply.github.com>
2023-06-20 09:28:58 +0800

    Add files via upload

---
+++ passgenerate.py
@@ -0,0 +1,16 @@
+from random import *
+import string
+
+letter = string.ascii_letters
+digits = string.digits
+special_char = string.punctuation
+
+alphabet = letter + digits + special_char
+
+pwd = ''
+pwd_length = int(input("Length Password : "))
+
+for i in range(pwd_length):
+    pwd += ''.join(choice(alphabet))
+
+print("\033[1;32mPassword \033[0m: ","\033[1;31m",pwd,"\033[0m")
