2818cccf94b461fd31a4c0e97b93032210a8e66a
blaxhades <122688263+blaxhades@users.noreply.github.com>
2024-03-02 16:40:24 +0800

    Update passgenerate.py

--- passgenerate.py
+++ passgenerate.py
@@ -13,4 +13,4 @@ pwd_length = int(input("Length Password : "))
 for i in range(pwd_length):
     pwd += ''.join(choice(alphabet))
 
-print("\033[1;32mPassword \033[0m: ","\033[1;31m",pwd,"\033[0m")
+print("\033[1;32mPassword \033[0m:","\033[1;31m",pwd,"\033[0m")
